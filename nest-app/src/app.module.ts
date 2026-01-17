b4e28b18380f5a31fb880cb8ec1a1e5a9a54b5b2
MinhNhut190705 <NhutLM.CE190737@gmail.com>
2026-01-17 19:51:49 +0700

    add env & config trong nest-app

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -3,21 +3,40 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArtistsModule } from './artists/artists.module';
 // newly added import
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AlbumsModule } from './albums/albums.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '12345',
-    database: 'Chinook_AutoIncrement',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: false,
-  })
-    , ArtistsModule, AlbumsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: configService.get<string>('DB_TYPE', 'mysql') as 'mysql',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        // host: 'db',
+        port: Number(configService.get<number>('DB_PORT', 3306)),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        entities: [],
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+    ArtistsModule,
+    AlbumsModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
