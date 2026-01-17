2853fdaded88e4f862858cae26573d278e789c04
MinhNhut190705 <NhutLM.CE190737@gmail.com>
2026-01-17 20:03:26 +0700

    update api

--- react-app/src/api/axiosClient.js
+++ react-app/src/api/axiosClient.js
@@ -10,4 +10,27 @@ const axiosClient = axios.create({
   }
 });
 
+// Đính token vào header
+axiosClient.interceptors.request.use (
+  (config) => {
+    const token = localStorage.getItem('access_token');
+    if(token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Xử lí dl trả về và bắt lỗi
+axiosClient.interceptors.response.use(
+  (response) => { return response.data;},
+  (error) => {
+    if(error.response?.status === 401) {
+      localStorage.removeItem('access_token');
+    }
+    return Promise.reject(error);
+  }
+)
+
 export default axiosClient; 
